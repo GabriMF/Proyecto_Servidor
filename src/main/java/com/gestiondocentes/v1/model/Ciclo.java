accc73529ff6c72f66a8237773dec23d3d37223e
Gabi <gabrimf25@gmail.com>
2025-12-03 13:04:09 +0100

    'Ciclo' entity created. Expecting to connect with 'Asignatura'

---
+++ src/main/java/com/gestiondocentes/v1/model/Ciclo.java
@@ -0,0 +1,33 @@
+package com.gestiondocentes.v1.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Column;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+public class Ciclo {
+    @Id
+    private Long id;
+    private String nombre;
+    private String familia;
+    private String codigo;
+}
